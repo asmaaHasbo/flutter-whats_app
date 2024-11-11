6125c669d29b4f4543e121bdb8b673b2751a9345
asmaa <elsaeeedasmaa@gmail.com>
2024-11-11 17:45:07 +0200

    add class on login screen

--- lib/features/auth/presentation/views/login_screen.dart
+++ lib/features/auth/presentation/views/login_screen.dart
@@ -1,1 +1,10 @@
-//---- commit for the test branch
+import 'package:flutter/cupertino.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
